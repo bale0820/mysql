5b579765155547eb7b063be8a7a91c9db00c70bb
bale <dbsgywls667@gmail.com>
2025-08-01 14:49:48 +0900

    파일 추가

--- mysql_80_실습.sql
+++ mysql_80_실습.sql
@@ -2002,7 +2002,10 @@ delimiter ;
 select * from department;
 
 
-
+-- Connection 확인
+show status like 'Threads_connected'; -- 접속 커넥션 수
+show processlist; 				      -- 활성중인 커넥션
+show variables like 'max_connections';-- 최대 접속 가능 커넥션 수
 
 
 
